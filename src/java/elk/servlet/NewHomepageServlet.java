4be33b44b9dabeda08f08ed5145b2eef68282434
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2018-11-15 18:14:24 +0700

    create NewHomepageServlet

--- src/java/elk/servlet/NewHomepageServlet.java
+++ src/java/elk/servlet/NewHomepageServlet.java
@@ -29,7 +29,19 @@ public class NewHomepageServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       getServletContext().getRequestDispatcher("/HomepageView.jsp").forward(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewHomepageServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewHomepageServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
