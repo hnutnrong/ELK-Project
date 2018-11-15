db296e36149e6d97f854691562341d2041b36774
Booksnatch <35445342+bookappleseed@users.noreply.github.com>
2018-11-15 16:35:54 +0700

    add productservlet code

--- src/java/elk/controller/ProductServlet.java
+++ src/java/elk/controller/ProductServlet.java
@@ -5,18 +5,29 @@
  */
 package elk.controller;
 
+import elk.jpa.controller.ProductJpaController;
+import elk.model.Product;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.UserTransaction;
 
 /**
  *
  * @author Administrator
  */
 public class ProductServlet extends HttpServlet {
+    @PersistenceUnit(unitName = "ElkfinalProjectPU")
+    EntityManagerFactory emf;
+    @Resource
+    UserTransaction utx;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,20 +40,11 @@ public class ProductServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProductServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProductServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+        ProductJpaController productJpa = new ProductJpaController(utx, emf);
+        List<Product> products= productJpa.findProductEntities();
+        getServletContext().getRequestDispatcher("/ProductView.jsp").forward(request, response);
+        
+                }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
